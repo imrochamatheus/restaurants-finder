14224044cdccaadfd42334aa17296af902af156b
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-05-22 10:44:09 -0300

    build: Instalado styled-reset

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { Reset } from "styled-reset";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Reset />
     <App />
   </React.StrictMode>
 );
